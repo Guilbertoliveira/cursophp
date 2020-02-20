d67c31be5e24b48eaa6bdc4156eeba70f09e058c
Guilbert Oliveira <41201436+Guilbertoliveira@users.noreply.github.com>
2020-02-20 13:38:41 -0300

    ultimo exercicio PHP

--- IntegracaoHTMLePHP 3/int3.php
+++ IntegracaoHTMLePHP 3/int3.php
@@ -1,9 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <?php  // pode colocar o php em cima também
+        $texto = isset($_GET["t"])?$_GET["t"]:"texto genetico";
+
+        $tamanho = isset($_GET["tam"])?$_GET["tam"]:"12pt";
+
+        $cor = isset($_GET["cor"])?$_GET["cor"]:"#0000000";
+    ?>
+
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Meuteste</title>
+    <style>
+        span.textu{
+            font-size: <?php echo $tamanho; ?>;
+            color: <?php echo $cor; ?>;
+        }
+
+    </style>
     <link rel="stylesheet" type="text/css" href="..\css\estilo.css"> 
 </head>
 
@@ -14,8 +30,8 @@
 <div>
     <?php
 
-
-
+    echo" <span class = 'textu'>$texto</span>;"
+    
         
             
             
