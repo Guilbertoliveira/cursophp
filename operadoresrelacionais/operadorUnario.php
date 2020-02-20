3bbf43713b13ad9f094b4bb76c4ef3659f5004a4
Guilbert Oliveira <41201436+Guilbertoliveira@users.noreply.github.com>
2020-02-20 11:37:33 -0300

    Resolvendo gambiarras

--- operadoresrelacionais/operadorUnario.php
+++ operadoresrelacionais/operadorUnario.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Meuteste</title>
+    <title>operacao</title>
     <link rel="stylesheet" type="text/css" href="..\css\estilo.css"> 
 </head>
 
@@ -13,7 +13,10 @@
 <h1>Testando aa</h1>
 <div>
     <?php
-
+    $n1 = $_GET["a"];
+    $n2 = $_GET["b"];
+    $tipo = $_GET["op"];
+    echo "os valores passados foram $n1 e $n2 <br>";
         
     
 
