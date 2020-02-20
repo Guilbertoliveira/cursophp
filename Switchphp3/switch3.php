65dbeaa9cc4f536b73189587a80a165a823ba265
Guilbert Oliveira <41201436+Guilbertoliveira@users.noreply.github.com>
2020-02-20 17:50:31 -0300

    Exercicio switch3

---
+++ Switchphp3/switch3.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Meuteste</title>
+    <link rel="stylesheet" type="text/css" href="css.css"> 
+</head>
+
+
+<body>
+
+<h1>SWITCH TESTE PHP3</h1>
+
+<div>
+    <?php
+
+    $seleciona = $_GET["sel"];
+
+    switch($seleciona)
+    {
+case 1:  echo"bbb"; break;
+case 2:  echo"bbb";break;
+case 3:  echo"bbb";break;
+case 4:  echo"bbb";break;
+case 5: echo"bbb";break;
+case 6: echo"bbb";break;
+case 7: echo"bbb";break;
+case 8: echo"bbb";break;
+case 9: echo"bbb";break;
+case 10: echo"bbb";break;
+case 11: echo"bbb";break;
+case 12: echo"bbb";break;
+case 13: echo"bbb";break;
+case 14: echo"bbb";break;
+case 15: echo"bbb";break;
+case 16: echo"bbb";break;
+case 17: echo"bbb";break;
+case 18: echo"bbb";break;
+case 19: echo"bbb";break;
+case 20: echo"bbb";break;
+case 21: echo"bbb";break;
+case 22: echo"bbb";break;
+case 23: echo"bbb";break;
+case 24: echo"bbb";break;
+case 25: echo"bbb";break;
+case 26: echo"Sao paulo";break;
+case 27: echo"bbb";break;
+case 28: echo"bbb";break;
+
+    }
+   
+   ?> 
+
+   <br><a href="javascript:history.go(-1)">volta</a>  <!-- voltando pagina com js !-->
+</div>
+    
+</body>
+</html>
