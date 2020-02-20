92cd9eb949682e22582bd8ac23017d1678bf750e
Guilbert Oliveira <41201436+Guilbertoliveira@users.noreply.github.com>
2020-02-20 12:06:48 -0300

    Finalizado os operadores

--- operadoresrelacionais/operadoresLogico.php
+++ operadoresrelacionais/operadoresLogico.php
@@ -10,11 +10,16 @@
 
 <body>
 
-<h1>Operadores relacionais</h1>
+<h1>Operadores logico</h1>
 <div>
     <?php
    
-   
+    $ano = $_GET["an"];
+    $idade = 2020 - $ano;
+    echo"Quem nasceu em ano de $ano tem idade de $idade anos";
+    //operador logico &&, ||, !
+    $tipo = ($idade >= 18 && $idade <=65)?"OBRIGATORIO":"NAO OBRIGATORIO";
+    echo" <BR> E DESSA FORMA SEU VOTO E: $tipo";
        
         
             
