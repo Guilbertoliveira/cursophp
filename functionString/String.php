5e5a56f62f22c91882e49ee38f51580bbc967d08
Guilbert Oliveira <41201436+Guilbertoliveira@users.noreply.github.com>
2020-02-21 15:01:39 -0300

    Comentando

--- functionString/String.php
+++ functionString/String.php
@@ -29,8 +29,21 @@
 
     $v2 = array(2,3,4,5);
     print_r($v2);
+    echo"<br>";
 
+    var_dump($v2); // mostra o tipo, quantos elementos possui (mais completo)
+    
+
+    // wordwrap cria quebra de linhas
+    $t = "exemplo para string gigantes que necessitam de quebra de linhaaaas";
+    $r = wordwrap($t, 20, "<br>", false); // oarametros : primerio o texto, numero de limitação, tag para quebra de linha visivel, false para quebrar palavras maiores que 20
+    echo($r);
+
+
+    echo"o tamanho e: ". strlen($t); //tamanho das strings (conta os espaços)
     ?>
+
+    
  
 
 </body>
